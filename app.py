afd2989e58df9619254f0fe73eca648f564df486
Dafny707 <avalosestefania678@gmail.com>
2026-05-19 02:09:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Student Mental Health")
 st.write("Visualización de datos sobre la salud mental en estudiantes")
 
-data = pd.read_csv("Student Mental health.csv")
+data = pd.read_csv("student-mental-health.csv")
 
 st.subheader("Datos")
 st.dataframe(data)
