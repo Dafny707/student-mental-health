8ddcc51a01d1dab2dad6cf1081b4a37910d20a54
Dafny707 <avalosestefania678@gmail.com>
2026-05-19 02:06:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
+import os
 import pandas as pd
 
-st.title("Student Mental Health")
-st.write("Visualización de datos sobre la salud mental en estudiantes")
+st.write("Archivos en la carpeta:")
+st.write(os.listdir())
 
 data = pd.read_csv("student_mental_health.csv")
-
-st.subheader("Datos")
 st.dataframe(data)
